1603cbdcab8a264a8146abec83778ac2a6c2b689
josefl <jose.fl_@hotmail.com>
2022-09-23 15:19:19 -0500

    Create user with Google Authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,6 @@ const login = async (req, res = response) => {
         // Generar el JWT
         const token = await generarJWT( usuario.id );
 
-
         res.json({
             msg: 'Login ok',
             usuario,
@@ -55,13 +54,40 @@ const googleSignIn = async (req, res = response) => {
     const { id_token } = req.body;
 
     try {
-        const googleUser = await googleVerify( id_token );
-        console.log(googleUser)
+        const { email, name, picture } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo: email });
+
+        if( !usuario ) {
+            // Si no existe el usuario, se crea.
+            const data = {
+                nombre: name,
+                correo: email,
+                password: 'Temp password :P',
+                img: picture,
+                google: true
+            }
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el estado del usuario es false (cuenta borrada), negar acceso
+        if( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Usuario bloqueado/no existe. Contacte a su administrador'
+            });
+        }
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id );
+
         
         res.json({
-            msg: 'Todo OK!',
-            id_token
-        })
+            usuario,
+            token
+        });
+        
     } catch (error) {
         return res.status(400).json({
             ok: false,
